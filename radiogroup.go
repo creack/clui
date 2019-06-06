d1f925dd6a2f66f9c417c40c77a70cb5e2b5cf3a
Guillaume J. Charmes <gcharmes@magicleap.com>
2019-06-06 18:13:17 -0400

    Add Active / SetActive to radiogroup
    Signed-off-by: Guillaume J. Charmes <gcharmes@magicleap.com>

--- radiogroup.go
+++ radiogroup.go
@@ -9,9 +9,7 @@ type RadioGroup struct {
 
 // CreateRadioGroup creates a new RadioGroup
 func CreateRadioGroup() *RadioGroup {
-	c := new(RadioGroup)
-	c.items = make([]*Radio, 0)
-	return c
+	return &RadioGroup{}
 }
 
 // Selected returns the number of currently selected radio
@@ -46,28 +44,52 @@ func (c *RadioGroup) SelectItem(r *Radio) bool {
 	return found
 }
 
-// SetSelected selects the radio by its number. The function
-// returns false if the number is invalid for the radio group
+// SetSelected selects the radio by its number.
+// Returns false if the number is invalid for the radio group.
 func (c *RadioGroup) SetSelected(id int) bool {
-	found := false
 	if id < 0 || id >= len(c.items) {
 		return false
 	}
-
-	for idx, item := range c.items {
-		if idx == id {
-			found = true
-			item.SetSelected(true)
-		} else {
-			item.SetSelected(false)
-		}
-	}
-
-	return found
+	return c.SelectItem(c.items[id])
 }
 
 // AddItem adds a new radio button to group
 func (c *RadioGroup) AddItem(r *Radio) {
 	c.items = append(c.items, r)
 	r.SetGroup(c)
+}
+
+// Item returns an item from the group.
+// Returns nil if the number is invalid for the radio group.
+func (c *RadioGroup) Item(id int) *Radio {
+	if id < 0 || id >= len(c.items) {
+		return nil
+	}
+	return c.items[id]
+}
+
+// SelectedItem returns the select item from the group.
+// Returns nil if the group is empty or if none are selected.
+func (c *RadioGroup) SelectedItem() *Radio {
+	return c.Item(c.Selected())
+}
+
+// Active checks if one of the element is active.
+func (c *RadioGroup) Active() bool {
+	for _, item := range c.items {
+		if item.Active() {
+			return true
+		}
+	}
+
+	return false
+}
+
+// SetActive sets the selected item to the given active value.
+// If none are active, fallback to parent.
+func (c *RadioGroup) SetActive(active bool) {
+	if item := c.SelectedItem(); item != nil {
+		item.SetActive(active)
+		return
+	}
 }
